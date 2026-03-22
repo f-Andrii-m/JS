2f481cca812d61426b2f092bf0d1fed51a3232a5
f-Andrii-m <145935728+f-Andrii-m@users.noreply.github.com>
2026-03-23 01:25:22 +0200

    Create SpeakGoodBye.js

---
+++ Lab_3/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+var byeSpeaker = {};
+
+(function (window) {
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
